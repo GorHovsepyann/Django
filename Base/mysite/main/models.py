ad3e211968aab63382c86ffccca4dccd6ca35bbb
GorHovsepyann <146917478+GorHovsepyann@users.noreply.github.com>
2023-12-12 20:31:19 +0400

    Add files via upload
    Django Basics

---
+++ Base/mysite/main/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+class brend(models.Model):
+    
+    name = models.CharField('Brend Name',max_length=50)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+    
+class car(models.Model):
+    
+    brend = models.ForeignKey(brend,on_delete=models.CASCADE,related_name='cat_prod')
+    name = models.CharField('Car name',max_length=50)
+    img = models.ImageField('Car image',upload_to='media/cars/')
+    price = models.PositiveIntegerField('Car price')
+    
+    def __str__(self) -> str:
+        return self.name
