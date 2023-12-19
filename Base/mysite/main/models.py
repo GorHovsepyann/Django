99a4655c3c902eaea9d72e7cdca049f046c38621
GorHovsepyann <146917478+GorHovsepyann@users.noreply.github.com>
2023-12-19 18:28:45 +0400

    Update models.py

--- Base/mysite/main/models.py
+++ Base/mysite/main/models.py
@@ -14,7 +14,7 @@ class car(models.Model):
     
     brend = models.ForeignKey(brend,on_delete=models.CASCADE,related_name='cat_prod')
     name = models.CharField('Car name',max_length=50)
-    img = models.ImageField('Car image',upload_to='media/cars/')
+    img = models.ImageField('Car image',upload_to='cars/')
     price = models.PositiveIntegerField('Car price')
     
     def __str__(self) -> str:
